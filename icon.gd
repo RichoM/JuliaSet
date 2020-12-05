341eaa2ed411ace435478feed153a99c791fcaf9
RichoM <richi.moran@gmail.com>
2020-12-05 12:05:09 -0300

    Improved script

--- icon.gd
+++ icon.gd
@@ -5,6 +5,9 @@ func _input(event):
 		var viewport = get_viewport_rect();
 		var mouse_x = event.position.x/viewport.size.x;
 		var mouse_y = event.position.y/viewport.size.y;
+		mouse_x = mouse_x * 4 - 2;
+		mouse_y = mouse_y * 4 - 2;
+		print(Vector2(mouse_x, mouse_y))
 		material.set_shader_param("real", mouse_x);
 		material.set_shader_param("imaginary", mouse_y);
    
